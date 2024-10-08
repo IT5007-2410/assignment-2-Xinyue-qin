b997eaf946e5666aa1bac662a29a6955b46ccbc6
Qinxinyue <qinxinyue@QinxinyuedeMacBook-Pro.local>
2024-10-08 20:12:29 +0800

    Q1 updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 const initialTravellers = [
   {
     id: 1, name: 'Jack', age: 80, gender: 'Male', phone: 88885555,
-    bookingTime: new Date()
+    bookingTime: new Date(), specialRequest: "No special request."
   },
   {
     id: 2, name: 'Rose', age: 25, gender: 'Female', phone: 88884444,
-    bookingTime: new Date()
+    bookingTime: new Date(), specialRequest: "No special request."
   },
 ];
 
