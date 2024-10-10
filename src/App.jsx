fce259409a8f236c097d8c95504de16acb286105
Qinxinyue <qinxinyue@QinxinyuedeMacBook-Pro.local>
2024-10-10 19:07:38 +0800

    Q5 updated

--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,11 @@ class Display extends React.Component {
 	/*Q3. Write code to render rows of table, reach corresponding to one traveller. Make use of the TravellerRow function that draws one row.*/
     const TravellerRows = traveller.map(i=><tr key={i.id}><td>{i.id}</td><td>{i.name}</td><td>{i.age}</td><td>{i.phone}</td><td>{i.email}</td><td>{i.bookingTime}</td></tr>); //add key = {i.id} (unique ID) here as the key
   return (
-    
-    <table className="bordered-table">
+    <div>
+    <h3>
+       {'Display Traveller'}
+    </h3>
+    <table className="Display Travellers">
       <thead>
         <tr>
 	  {/*Q3. Below table is just an example. Add more columns based on the traveller attributes you choose.*/}
@@ -52,6 +55,7 @@ class Display extends React.Component {
         TravellerRows}
       </tbody>
     </table>
+    </div>
   );
   }
 }
@@ -73,6 +77,10 @@ class Add extends React.Component {
 
   render() {
     return (
+      <div>
+      <h3>
+         {'Add Traveller'}
+      </h3>
       <form name="bookTraveller" onSubmit={this.handleSubmit}>
 	    {/*Q4. Placeholder to enter passenger details. Below code is just an example.*/}
         <input type="text" name="travellername" placeholder="Name" />
@@ -82,6 +90,7 @@ class Add extends React.Component {
         <input type="date" name="travellerbookingTime" placeholder="BookingTime" />
         <button>Add</button>
       </form>
+      </div>
     );
   }
 }
@@ -103,11 +112,16 @@ class Delete extends React.Component {
 
   render() {
     return (
+      <div>
+      <h3>
+         {'Delete Traveller'}
+      </h3>
       <form name="deleteTraveller" onSubmit={this.handleSubmit}>
 	    {/*Q5. Placeholder form to enter information on which passenger's ticket needs to be deleted. Below code is just an example.*/}
 	<input type="text" name="travellername" placeholder="Name" />
         <button>Delete</button>
       </form>
+      </div>
     );
   }
 }
@@ -117,16 +131,61 @@ class Homepage extends React.Component {
 	super();
 	}
 	render(){
+    const passengerColor = this.props.passengerColor;
+    //const emptySeats = 10 - passengerNumber;
+    //const passengerColor = ['green'] * emptySeats + ['grey'] * passengerNumber;
+    console.log(passengerColor);
+    /*Q3. Write code to render rows of table, reach corresponding to one traveller. Make use of the TravellerRow function that draws one row.*/
+    //const seatMapRows =passengerColor.map(i=><tr key={Math.random()} style={{backgroundColor: i}}><td> </td></tr>);
+    //const seatMapRows =<tr><td style={{backgroundColor: passengerColor[0]}}></td></tr>;
+    const seatMapRows =<tr>
+      <td bgcolor = {passengerColor[0]}></td>
+    <td bgcolor = {passengerColor[1]}></td>
+    <td bgcolor = {passengerColor[2]}></td>
+    <td bgcolor = {passengerColor[3]}></td>
+    <td bgcolor = {passengerColor[4]}></td>
+    <td bgcolor = {passengerColor[5]}></td>
+    <td bgcolor = {passengerColor[6]}></td>
+    <td bgcolor = {passengerColor[7]}></td>
+    <td bgcolor = {passengerColor[8]}></td>
+    <td bgcolor = {passengerColor[9]}></td></tr>;
+    console.log(passengerColor);
 	return (
 	<div>
 		{/*Q2. Placeholder for Homepage code that shows free seats visually.*/}
-	</div>);
+    <h3>
+         {'Visual Representation of reserved/unreserved tickets.'}
+    </h3>
+    <table className="Display Travellers" width="300">
+      <thead>
+        <tr>
+	  {/*Q3. Below table is just an example. Add more columns based on the traveller attributes you choose.*/}
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+          <th>4</th>
+          <th>5</th>
+          <th>6</th>
+          <th>7</th>
+          <th>8</th>
+          <th>9</th>
+          <th>10</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/*Q3. write code to call the JS variable defined at the top of this function to render table rows.*/
+        seatMapRows}
+      </tbody>
+    </table>
+
+	</div>
+  );
 	}
 }
 class TicketToRide extends React.Component {
   constructor() {
     super();
-    this.state = {travellers: [], showSelectorCount: 0, passengerNumber: 0};
+    this.state = {travellers: [], showSelectorCount: 0, passengerNumber: 0, passengerColor: ['green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'grey', 'grey']};
     this.showSelector = this.showSelector.bind(this); //"bind" method is used to pass the context (e.g. "this") to javascript function when it will trigger.
     this.bookTraveller = this.bookTraveller.bind(this);
     this.deleteTraveller = this.deleteTraveller.bind(this);
@@ -147,7 +206,7 @@ class TicketToRide extends React.Component {
   }
   loadData() {
     setTimeout(() => {
-      this.setState({travellers: initialTravellers, ID: currentID, passengerNumber: 2});
+      this.setState({travellers: initialTravellers, ID: currentID, passengerNumber: 2, passengerColor: ['green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'grey', 'grey']});
     }, 500);
   }
 
@@ -157,6 +216,7 @@ class TicketToRide extends React.Component {
     //actural add
     var ID = this.state.ID + 1;
     var passengerNumber = this.state.passengerNumber + 1;
+    var passengerColor = [];
     var newpassenger =  {
       id: ID, name: passengername, age: passengerage, phone: passengerphone,
       email: passengeremail, bookingTime: passengerbookingTime
@@ -164,9 +224,23 @@ class TicketToRide extends React.Component {
     if (passengerNumber > 10) { //overflow case assuming that the train has 10 seats in total
       alert("No free seats now!");
     } else {
+      if (passengerNumber < 10){
+        for (let i = 0; i < 10-passengerNumber; i++) {
+          passengerColor.push('green');
+      }
+        for (let i = 0; i < passengerNumber; i++) {
+          passengerColor.push('grey');
+      }
+    }
+      else {      
+        for (let i = 0; i < 10-passengerNumber; i++) {
+          passengerColor.push('grey');
+        }
+      }
       this.state.travellers.push(newpassenger);
-      this.setState({ID: ID, passengerNumber: passengerNumber});
+      this.setState({ID: ID, passengerNumber: passengerNumber, passengerColor: passengerColor});
       alert("Added!");
+
     }
     //this.state.travellers.push(newpassenger);
     //this.setState({ID: ID, passengerNumber: passengerNumber});
@@ -187,34 +261,32 @@ class TicketToRide extends React.Component {
     var newlist = [];
     var newpassengerNumber = 0;
     var passengerNumber = this.state.passengerNumber;
-    if (passengerNumber < 1) { //overflow case assuming that the train has 10 seats in total
-      alert("No passengers to delete now!");
+    if (passengerNumber < 1) {//overflow case assuming that the train has 10 seats in total
+      alert("No passenger could be deleted now!");
     } else {
-      this.state.travellers.forEach(element => {
-        if (element.name != passenger){
-          newlist.push(element);
+      this.state.travellers.forEach(i => {
+        if (i.name != passenger){
+          newlist.push(i);
           newpassengerNumber = newpassengerNumber + 1};
       });
-      this.setState({travellers:newlist})
+      this.setState({travellers:newlist});
       console.log(this.state.travellers);
       console.log(newlist)
         if (newpassengerNumber == passengerNumber){
-          alert("Passengers not found!");
+          alert("Sorry, fail to find passenger!");
         }
         else{
           this.setState({passengerNumber: newpassengerNumber});
-          alert("Deleted!");
+          alert("Deletion Done!");
         }
-      
-  /////////////////////////
-    }
+      }
 
-    this.state.travellers.forEach(element => {
-      if (element.name != passenger){newlist.push(element)};
-    });
-    this.setState({travellers:newlist})
-    console.log(this.state.travellers);
-    console.log(newlist)
+    // this.state.travellers.forEach(element => {
+    //   if (element.name != passenger){newlist.push(element)};
+    // });
+    // this.setState({travellers:newlist})
+    // console.log(this.state.travellers);
+    // console.log(newlist)
 
   }
   
@@ -228,7 +300,7 @@ class TicketToRide extends React.Component {
 
         <nav>
             <ul className = "nav_menu">
-              <li onClick={() => this.showSelector(0)}><a href = "#">Home</a></li>
+              <li onClick={() => this.showSelector(0)}><a href = "#">Homepage</a></li>
               <li onClick={() => this.showSelector(1)}><a href = "#">Display</a></li>
               <li onClick={() => this.showSelector(2)}><a href = "#">Add</a></li>
               <li onClick={() => this.showSelector(3)}><a href = "#">Delete</a></li>
@@ -239,7 +311,7 @@ class TicketToRide extends React.Component {
 	      <div>
 		      {/*Only one of the below four divisions is rendered based on the button clicked by the user.*/}
 		      {/*Q2 and Q6. Code to call Instance that draws Homepage. Homepage shows Visual Representation of free seats.*/}
-          {showSelectorCount == 0 ? <Add addfunction={this.bookTraveller}/> : null}
+          {showSelectorCount == 0 ? <Homepage passengerColor={this.state.passengerColor}/> : null}
           		      {/*Q3. Code to call component that Displays Travellers.*/}
           {showSelectorCount == 1 ? <Display travellerprop={this.state.travellers}/> : null}
                     		      {/*Q4. Code to call the component that adds a traveller.*/}
